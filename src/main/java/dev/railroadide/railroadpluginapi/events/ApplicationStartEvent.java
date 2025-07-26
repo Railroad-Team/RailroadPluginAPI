0fa1fb17fab69b30ee534bfff34b6e6b899c3b13
TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2025-07-26 12:46:07 +0100

    Add application start and stop events
    Introduced ApplicationStartEvent and ApplicationStopEvent to signal application lifecycle events. These can be used for initialization and cleanup tasks in plugins.

---
+++ src/main/java/dev/railroadide/railroadpluginapi/events/ApplicationStartEvent.java
@@ -0,0 +1,9 @@
+package dev.railroadide.railroadpluginapi.events;
+
+import dev.railroadide.railroadpluginapi.event.Event;
+
+/**
+ * This event is triggered when the application starts.
+ * It can be used to perform initialization tasks or to set up resources needed by the application.
+ */
+public class ApplicationStartEvent implements Event {}
---
+++ src/main/java/dev/railroadide/railroadpluginapi/events/ApplicationStopEvent.java
@@ -0,0 +1,9 @@
+package dev.railroadide.railroadpluginapi.events;
+
+import dev.railroadide.railroadpluginapi.event.Event;
+
+/**
+ * Event that is fired when the application is stopping.
+ * This event can be used to perform cleanup operations before the application exits.
+ */
+public class ApplicationStopEvent implements Event {}
